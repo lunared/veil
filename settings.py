ca0ddc9405afaa1da5d31add005dfd675bf96283
Nicholas Hydock <nhydock@gmail.com>
2018-04-18 19:36:53 -0400

    load variables from the environment

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
-AMQP_URL = "amqp://guest:guest@127.0.0.1/"
-REDIS_URL = ('localhost', 6379)
+import os
+
+AMQP_URL = os.environ.get("AMQP_URL", "amqp://guest:guest@127.0.0.1/")
+REDIS_URL = os.environ.get("REDIS_URL", ('localhost', 6379))
 GEOIP_PATH = './GeoLite2-City.mmdb'
 
 CORS_SUPPORTS_CREDENTIALS = True
